9ba84462162d4a193d7711b456b7b8939c91614a
Damian <damianeldiez@gmail.com>
2020-08-04 12:03:46 -0300

    supermerk2.py

--- supermerk2.py
+++ supermerk2.py
@@ -1,5 +1,5 @@
 class producto:
-    def __init(self, nombre, precio,primera_necesidad):
+    def __init__(self, nombre, precio,primera_necesidad):
         self.__nombre=nombre
         self.__precio=precio
         self.__primera_necesidad=primera_necesidad
@@ -12,3 +12,18 @@ class producto:
         if primera_necesidad
         else:
             return self._precio
+
+class supermercado:
+    def __init__(self, nombre, direccion):
+        self.__nombre=nombre
+        self.__direccion=direccion
+        self.lista_productos=[]
+    
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_direccion(self):
+        return self.__direccion
+
+    def set_lista_productos(self,producto):
+        self.__lista_productos.append(producto)
