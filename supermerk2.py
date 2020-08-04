c6dcbd23573139883f0352f91400e92176a5e2b4
Damian <damianeldiez@gmail.com>
2020-08-04 11:16:25 -0300

    supermerk2.py

--- supermerk2.py
+++ supermerk2.py
@@ -1,2 +1,14 @@
-def producto():
-    init__
+class producto:
+    def __init(self, nombre, precio,primera_necesidad):
+        self.__nombre=nombre
+        self.__precio=precio
+        self.__primera_necesidad=primera_necesidad
+
+    def get_nombre(self):
+        return self.__nombre
+    def get primera_necesidad (self):
+        return self.__primera_necesidad
+    def precio(self):
+        if primera_necesidad
+        else:
+            return self._precio
